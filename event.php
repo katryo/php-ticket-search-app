e8c6193c80ebcfe219eeffd9e83ddc82b9e4c405
katryo <katoryo55@gmail.com>
2018-10-15 21:56:26 -0700

    implement handle here/there

--- event.php
+++ event.php
@@ -12,19 +12,42 @@ function do_it($something){
     <style>
         body {
             margin: auto;
-            width: 1000px;
+            width: 800px;
         }
 
         .event-search {
-            background: #eee;
+            margin-top: 40px;
+            background: #fafafa;
             border-style: solid;
-            border-color: gray;
+            border-color: #eee;
+            padding: 5px;
+        }
+
+        .event-search-heading {
+            text-align: center;
+            font-style: italic;
         }
     </style>
+    <script>
+        'use strict';
+
+        function handleThere() {
+            const input = window.document.getElementById('js-input-location');
+            input.required = true;
+            input.disabled = false;
+        }
+
+        function handleHere() {
+            const input = window.document.getElementById('js-input-location');
+            input.required = false;
+            input.disabled = true;
+        }
+
+    </script>
 </head>
 <body>
 <div class="event-search">
-    <h1>Events Search</h1>
+    <h1 class="event-search-heading">Events Search</h1>
     <hr>
     <form action="#" method="get">
         <div>
@@ -35,6 +58,7 @@ function do_it($something){
         <div>
             <label for="category">Category</label>
             <select name="category" id="category">
+                <option value="default">Default</option>
                 <option value="music">Music</option>
                 <option value="sports">Sports</option>
                 <option value="arts">Arts</option>
@@ -49,13 +73,14 @@ function do_it($something){
             <input type="text" name="distance">
 
             <label for="from">from</label>
-            <input type="radio" name="from" value="here"> here<br>
-            <input type="radio" name="from" value="there">
-            <input type="text" name="location" placeholder="location">
+            <input type="radio" name="from" value="here" onclick="handleHere();"> here<br>
+            <input type="radio" name="from" value="there" onclick="handleThere();">
+
+            <input type="text" name="location" placeholder="location" id="js-input-location">
         </div>
         
         <div>
-            <input type="submit" value="Search">
+            <input type="submit" value="Search" disabled>
             <button>Clear</button>
         </div>
 
