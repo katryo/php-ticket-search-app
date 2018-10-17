17e57555c850bc4c33fb1a3db8e72f2664c18adf
katryo <katoryo55@gmail.com>
2018-10-17 10:38:13 -0700

    start dev table

--- event.php
+++ event.php
@@ -1,8 +1,70 @@
 
 <?php
+include 'geoHash.php';
+
 function do_it($something){
     return $something . "abc";
 }
+
+function fetch_location($address) {
+    $app_key = getenv('G_MAP_API_KEY');
+    $url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($address) . '&key=' . $app_key;
+    $response = file_get_contents($url);
+    return $response;
+}
+
+function search_events($geo_point, $keyword, $segment, $distance) {
+    $consumer_key = getenv('TICKET_CONSUMER_KEY');
+    $segment_id = '';
+
+    if ($segment == 'default') {
+        $url = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey='
+            . $consumer_key
+            . '&keyword='
+            . urlencode($keyword)
+            . '&radius='
+            . $distance
+            . '&unit=miles&geoPoint='
+            . $geo_point;
+    } else {
+        switch ($segment) {
+
+            case 'music':
+                $segment_id = 'KZFzniwnSyZfZ7v7nJ';
+                break;
+
+            case 'sports':
+                $segment_id = 'KZFzniwnSyZfZ7v7nE';
+                break;
+
+            case 'arts':
+                $segment_id = 'KZFzniwnSyZfZ7v7na';
+                break;
+
+            case 'film':
+                $segment_id = 'KZFzniwnSyZfZ7v7nn';
+                break;
+
+            case 'miscellaneous':
+                $segment_id = 'KZFzniwnSyZfZ7v7n1';
+                break;
+        }
+
+        $url = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey='
+            . $consumer_key
+            . '&keyword='
+            . urlencode($keyword)
+            . '&segmentId='
+            . $segment_id
+            . '&radius='
+            . $distance
+            . '&unit=miles&geoPoint='
+            . $geo_point;
+    }
+    $response = file_get_contents($url);
+    return $response;
+}
+
 ?>
 
 <html>
@@ -34,6 +96,10 @@ function do_it($something){
 
         .buttons {
             margin-left: 120px;
+        }
+
+        .icon {
+            width: 100px;
         }
         
         label {
@@ -94,8 +160,67 @@ function do_it($something){
             window.document.getElementById('js-submit').disabled = false;
         }
 
+        function getEventsOnPage() {
+            const eventsStr = document.getElementById('js-events-response').innerText;
+            if (eventsStr) {
+                return JSON.parse(eventsStr)._embedded.events;
+            } else {
+                return [];
+            }
+        }
+
+        function generateTr(event) {
+            const tr = document.createElement('tr');
+
+            const localDate = event.dates.start.localDate;
+            const localTime = event.dates.start.localTime;
+            const tdDate = document.createElement('td');
+            tdDate.innerText = localDate + ' ' + localTime;
+            tr.appendChild(tdDate);
+
+            const images = event.images;
+            const tdImage = document.createElement('td');
+            if (images.length > 0) {
+                const iconUrl = event.images[0].url;
+                const image = document.createElement('img');
+                image.src = iconUrl;
+                image.class = 'icon';
+                tdImage.appendChild(image);
+            } else {
+                tdImage.innerText = 'N/A';
+            }
+            tr.appendChild(tdImage);
+
+            const tdName = document.createElement('td');
+            tdName.innerText = event.name;
+            tr.appendChild(tdName);
+
+            return tr;
+        }
+
+        function renderEvents(events) {
+            const table = document.createElement('table');
+
+            for (let headName of ['Date', 'Icon', 'Event', 'Genre', 'Venue']) {
+                const th = document.createElement('th');
+                th.innerText = headName;
+                table.appendChild(th);
+            }
+
+            for (let event of events) {
+                console.log(event);
+                const tr = generateTr(event);
+                table.appendChild(tr);
+            }
+            document.getElementById('search-results').appendChild(table);
+        }
+
         window.onload = function() {
             startLocating();
+            const events = getEventsOnPage();
+            if (events.length > 0) {
+                renderEvents(events);
+            }
         }
 
     </script>
@@ -141,11 +266,35 @@ function do_it($something){
             <button onclick="handleClear(); return false;">Clear</button>
         </div>
 
+        <div id="search-results"></div>
+
     </form>
 
-    <?php if ($_GET['keyword']) {; ?>
-        aieee
-    <?php }; ?>
+    <?php
+
+    if ($_GET['keyword']) {
+        $lat = '';
+        $lon = '';
+
+        if ($_GET['from'] == 'there') {
+            $address = $_GET['location'];
+            $response = fetch_location($address);
+            if ($response) {
+            } else {
+                $location = $response['results'][0]['geometry']['location'];
+                $lat = $location['lat'];
+                $lon = $location['lon'];
+            }
+        } else {
+            $lat = $_GET['this-lat'];
+            $lon = $_GET['this-lon'];
+        }
+        $geo_point = encode(floatval($lat), floatval($lon));
+        $search_results = search_events($geo_point, $_GET['keyword'], $_GET['category'], (int)$_GET['distance']);
+        echo '<div id="js-events-response" style="display: none">' . $search_results . '</div>';
+    }
+
+    ?>
 
     <p><?php echo do_it('abced') ?></p>
 
